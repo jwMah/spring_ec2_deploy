e0972e106bb1cc5bff7a78f32d93dbd45f1dbed0
jwMah <58331108+jwMah@users.noreply.github.com>
2021-11-06 16:17:58 +0900

    Add files via upload

---
+++ demo/src/main/java/com/jaewan/demo/JaewanApplication.java
@@ -0,0 +1,13 @@
+package com.jaewan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaewanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaewanApplication.class, args);
+	}
+
+}
